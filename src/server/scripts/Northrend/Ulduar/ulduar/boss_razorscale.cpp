b2cd11e85219ef841915f1c1f187a56dc17cedb5
Brian <runningnak3d@gmail.com>
2011-05-15 09:43:25 -0400

    Misc: Update copright header

--- src/server/scripts/Northrend/Ulduar/ulduar/boss_razorscale.cpp
+++ src/server/scripts/Northrend/Ulduar/ulduar/boss_razorscale.cpp
@@ -1,5 +1,6 @@
 /*
-* Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
+* Copyright (C) 2011 SingularityCore <http://www.singularitycore.org/>
+ * Copyright (C) 2008-2011 TrinityCore <http://www.trinitycore.org/>
 *
 * This program is free software; you can redistribute it and/or modify it
 * under the terms of the GNU General Public License as published by the
